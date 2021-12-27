cbf87ce58d11079550c68aa5fd9f5516fcf4fe96
gamxu <wuttipat.did@gmail.com>
2021-12-27 16:32:55 +0700

    lab14_1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,32 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[]){
+    
+    double sum,SDsum,geoProd=1,harDvds,max=a[0],min=a[0];
+    for(int i=0; i<n; i++){
+      sum += a[i];
+    }
+    b[0] = sum/n;
+
+    for(int j=0;j<n;j++){
+      SDsum += pow(a[j]-b[0],2);
+    }
+    b[1] = sqrt(SDsum/n);
+   
+    for(int k =0; k<n;k++){
+      geoProd *= a[k];
+    }
+    b[2] = pow(geoProd,(double) 1/n);
+
+    for(int l=0; l<n;l++){
+    harDvds += 1/a[l];
+    }
+    b[3] = n/harDvds;
+
+    for(int m=0; m<n; m++){
+      if(a[m]>max) b[4]=a[m];
+      if(a[m]<min) b[5]=a[m];
+    }
+}
