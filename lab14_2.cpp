2bfed6d18883be5008b1c25ef344d4d5732e2991
gamxu <wuttipat.did@gmail.com>
2021-12-27 16:31:24 +0700

    lab14_2 completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,39 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool I[][M],int s,int x,int y)
+{
+    for(int i=0; i<N;i++)
+    {
+        for(int j=0;j<M;j++)
+        {
+            if( sqrt(pow(i-x,2)+pow(j-y,2)) <= s-1 )
+            {
+                I[i][j] = 1;
+            }
+        }
+    }
+}
+
+void showImage(const bool I[][M])
+{
+    for(int i=0;i<M+2;i++) cout << "-";
+    cout << "\n";
+
+    for(int i=0; i<N;i++)
+    {
+        cout << "|";
+        for(int j=0;j<M;j++)
+        {
+            if(I[i][j]== 1)
+            {
+                cout << "*";
+            }else cout << " ";
+        }
+        cout << "|\n";
+    }
+    
+    for(int i=0;i<M+2;i++) cout << "-";
+    cout << "\n";
+}
